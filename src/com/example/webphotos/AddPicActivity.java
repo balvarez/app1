aa8acfbc1a394f26d466fa69107f178c99a9b56a
jmparker <jmparker@mit.edu>
2013-04-17 22:59:55 -0400

    upload photo is so fucking close. just doesn't display a preview of the picture you took

--- src/com/example/webphotos/AddPicActivity.java
+++ src/com/example/webphotos/AddPicActivity.java
@@ -17,16 +17,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class AddPicActivity extends Activity {
 	//could fairly simply allow for phones without a camera to use the app but not a priority
 	
-	//SO FAR this should launch the camera and come back and preview the picture taken
-	//TODO pick a restaurant
-	//TODO upload photo and rating to server
+	//SO FAR this should launch the camera and come back and preview the picture taken but it doesn't preview
+	//debugging this shit is hard because of weird usb mass storage interactions
+	//usb takes over the memory so that the camera can't write to files but other usb connections don't talk to logcat
+	//something in onActivityResult isn't working but I can't debug so I have no idea what
+	//TODO upload photo and rating to server. started method sendToServer at bottom.
 	//TODO make a submit graphic to replace the toMap graphic
 	
 	static RestaurantData restaurant;
@@ -34,19 +40,24 @@ public class AddPicActivity extends Activity {
 	String currentPhotoPath;
 	int pictureActionCode = 42;
 	ImageView preview;
+	static TextView restName;
+	RatingBar stars;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Intent intent = getIntent();
-		if (intent.hasExtra("restaurant")) {restaurant = (RestaurantData) intent.getSerializableExtra("restaurant");}
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_add_pic);
+		if (intent.hasExtra("restaurant")) {setRest((RestaurantData) intent.getSerializableExtra("restaurant"));}
 		else {
 			restList = (ListOfRestaurants) intent.getSerializableExtra("restaurantList");
 			DialogFragment picker = new RestaurantSelectDialog();
 			picker.show(getFragmentManager(), "restPicker");
+			Log.d("dialog", "where my dialog at");
 		}
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_pic);
 		preview = (ImageView)findViewById(R.id.newPhoto);
+		restName = (TextView)findViewById(R.id.addPicRestName);
 		try {
 			createImageFile();
 		} catch (IOException e) {
@@ -54,6 +65,10 @@ public class AddPicActivity extends Activity {
 			e.printStackTrace();
 		}
 		dispatchTakePictureIntent(pictureActionCode);
+		restName.setText("bloop");
+		restName.setTextColor(Color.parseColor("#D60000"));
+		restName.setTextSize(20);
+		preview.setImageDrawable(getResources().getDrawable(R.drawable.burger));
 	}
 	
 	@Override
@@ -64,7 +79,9 @@ public class AddPicActivity extends Activity {
 			if (resultCode == RESULT_OK) {
 				galleryAddPic();
 				Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
-				preview.setImageBitmap(bitmap);
+				Log.d("bitmap", "bitmap created");
+				this.preview.setImageBitmap(bitmap);
+				Log.d("bitmap", "preview should be showing");
 			}
 		}
 	}
@@ -80,14 +97,9 @@ public class AddPicActivity extends Activity {
 		// Create a unique image file name
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 		String imageFileName = "cs" + timeStamp + "_";
-		Log.d("imagefile", "made name");
-		File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "CuisineStream");
-		Log.d("imagefile", "created album");
-		File image = File.createTempFile(imageFileName, ".jpg",
-				directory);
-		Log.d("imagefile", "created temp file");
+//		File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "CuisineStream");
+		File image = File.createTempFile(imageFileName, ".jpg", this.getCacheDir());
 		currentPhotoPath = image.getAbsolutePath();
-		Log.d("imagefile", "absPath: "+currentPhotoPath);
 		return image;
 	}
 	
@@ -112,12 +124,24 @@ public class AddPicActivity extends Activity {
 				
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					restaurant = fullList[which];
+					Log.d("which", "which is "+which);
+					setRest(fullList[which]);
+					Log.d("restCheck", "rest: "+restaurant.toString());
 				}
 			});
 			return builder.create();
 		}
 	}
+	
+	private void sendToServer() {
+		double rating = stars.getRating();
+		String name = restaurant.name;
+		String location = Double.toString(restaurant.lat) + "+" + Double.toString(restaurant.lng);
+		String id = restaurant.id;
+		
+	}
+	
+	public static void setRest(RestaurantData rest) {restaurant = rest; restName.setText(restaurant.name);}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
